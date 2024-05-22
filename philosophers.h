ddf7ac37a571755e08e588265b3c48e894269504
Brayden Tomlinson <humortronic@gmail.com>
2024-05-22 16:31:12 +0930

    Added changes to code

---
+++ philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 16:26:10 by btomlins          #+#    #+#             */
+/*   Updated: 2024/05/22 16:29:16 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <limits.h>
+
