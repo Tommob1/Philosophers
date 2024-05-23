1fd000bca97e2a07ac66700579d9e286c9ab81a5
Brayden Tomlinson <humortronic@gmail.com>
2024-05-23 13:15:06 +0930

    Added changes to code

--- inc/philosophers.h
+++ inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:26:10 by btomlins          #+#    #+#             */
-/*   Updated: 2024/05/22 16:31:36 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/05/23 13:14:49 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,11 @@
 #include <pthread.h>
 #include <sys/time.h>
 #include <limits.h>
+
+# define PH_MAX 200
+
+# define RESET  "\033[0m"
+# define RED    "\033[1;31m"
+# define GREEN  "\033[1;32"
 
 #endif
