d99693ac45a15c4fafa66271f8db25a981da78af
Brayden Tomlinson <humortronic@gmail.com>
2024-05-23 15:15:35 +0930

    Added changes to code

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:21:56 by btomlins          #+#    #+#             */
-/*   Updated: 2024/05/23 13:31:18 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/05/23 15:14:39 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int	main(int argc,  char **argv)
 
     if (argc == 5 || argc == 6)
     {
-        
+        data.error_flag = 0;
+        parse_input(&data, argv);
     }
 
     return (0);
