c0e7d0874617fe9057ccd63fe94c82e07135a4dd
Brayden Tomlinson <humortronic@gmail.com>
2024-05-23 13:30:13 +0930

    Added changes to code

--- inc/philosophers.h
+++ inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:26:10 by btomlins          #+#    #+#             */
-/*   Updated: 2024/05/23 13:14:49 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/05/23 13:29:54 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,18 @@
 # define RESET  "\033[0m"
 # define RED    "\033[1;31m"
 # define GREEN  "\033[1;32"
+
+typedef struct s_data   t_data;
+
+typedef struct s_data
+{
+    long    ph_total;
+    long    time_to_die;
+    long    time_to_eat;
+    long    time_to_sleep;
+    long    meals_total;
+    long    start_time;
+    long    end_time;
+}           t_data;
 
 #endif
