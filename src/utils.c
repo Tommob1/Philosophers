d6197282f5b731b80a54095931366f346f90bb4c
Brayden Tomlinson <humortronic@gmail.com>
2024-06-05 15:49:57 +0930

    Added changes to code

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,26 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 15:46:11 by btomlins          #+#    #+#             */
-/*   Updated: 2024/06/05 15:46:15 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/06/05 15:49:36 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/philosophers.h"
+
+void	error_msg(const char	*error_msg)
+{
+	printf(MAGENTA "%s\n" RESET, error_msg);
+}
+
+void	*ft_malloc(size_t bytes)
+{
+	void	*retrieve;
+
+	retrieve = malloc(bytes);
+	if (retrieve == NULL)
+	{
+		error_msg("Memory allocation unsuccessful");
+		return (NULL);
+	}
+	return (retrieve);
+}
