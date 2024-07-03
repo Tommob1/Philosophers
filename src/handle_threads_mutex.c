7c4f60507cdf68973aa6cedb30fe18b09a18ddae
Brayden Tomlinson <humortronic@gmail.com>
2024-07-03 15:51:36 +0930

    Added changes to code

--- src/handle_threads_mutex.c
+++ src/handle_threads_mutex.c
@@ -6,7 +6,25 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:25 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/03 15:45:32 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/07/03 15:51:04 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/philosophers.h"
+
+static void	mutex_error_check(int status, t_ftcode ftcode)
+{
+	if (status != 0 && (ftcode == LOCK || ftcode == UNLOCK
+			|| ftcode == INIT || ftcode == DESTROY))
+	{
+			error_msg("Metex error")
+			return ;
+	}
+}
+
+void	handle_mutex(t_mtx *mtx, t_ftcode ftcode)
+{
+	if (ftcode == LOCK)
+		mutex_error_check(pthread_mutex_lock(mtx), ftcode);
+	else if (ftcode == UNLOCK)
+}
