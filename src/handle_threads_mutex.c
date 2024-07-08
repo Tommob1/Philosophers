252480d789aed288790f0015072e4d6182df2b51
Brayden Tomlinson <humortronic@gmail.com>
2024-07-08 16:15:36 +0930

    Added changes

--- src/handle_threads_mutex.c
+++ src/handle_threads_mutex.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:25 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/03 15:51:56 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/07/08 16:15:14 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,14 @@ void	handle_mutex(t_mtx *mtx, t_ftcode ftcode)
 	if (ftcode == LOCK)
 		mutex_error_check(pthread_mutex_lock(mtx), ftcode);
 	else if (ftcode == UNLOCK)
+		mutex_error_check(pthread_mutex_unlock(mtx), ftcode);
+	else if (ftcode == INIT)
+		mutex_error_check(pthread_mutex_init(mtx, NULL), ftcode);
+	else if (ftcode == DESTROY)
+		mutex_error_check(pthread_mutex_destroy(mtx), ftcode);
+	else
+	{
+		error_msg("ftcode options: LOCK, UNLOCK, INIT, DESTROY");
+		return ;
+	}
 }
