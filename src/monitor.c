5aac2d3ae1fcd204a2d8a2f674967391a62bff46
Brayden Tomlinson <humortronic@gmail.com>
2024-07-08 16:18:24 +0930

    Added changes

--- src/monitor.c
+++ src/monitor.c
@@ -6,13 +6,13 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 14:11:27 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/03 15:45:34 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/07/08 16:16:51 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
 
-void    wait_all_threads(t_data *data)
+void	wait_all_threads(t_data *data)
 {
     while (!get_bool(&data->access_mutex, &data->threads_ready))
             ;
