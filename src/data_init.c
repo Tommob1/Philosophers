b95f77eba65c40ae694cd811867d481374453ea0
Brayden Tomlinson <humortronic@gmail.com>
2024-07-16 17:03:37 +0930

    Added changes to code

--- src/data_init.c
+++ src/data_init.c
@@ -6,7 +6,28 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:27 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/03 15:45:31 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/07/16 17:03:17 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/philosophers.h"
+
+void	*single_philo(void *index)
+{
+
+}
+
+static void	assign_forks(t_ph *philo, t_fork *forks_arr, int ph_index)
+{
+
+}
+
+static void	philo_init(t_data *data)
+{
+
+}
+
+void	data_init(t_data *data)
+{
+
+}
