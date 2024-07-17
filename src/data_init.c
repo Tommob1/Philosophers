bb02928bbf68faab32f1b3ee0131e3792b379d62
Brayden Tomlinson <humortronic@gmail.com>
2024-07-17 14:53:17 +0930

    Added changes to code

--- src/data_init.c
+++ src/data_init.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:27 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/17 14:26:38 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/07/17 14:52:56 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,19 @@ void	*single_philo(void *index)
 
 static void	assign_forks(t_ph *philo, t_fork *forks_arr, int ph_index)
 {
+	int	ph_total;
 
+	ph_total = philo->data->ph_total;
+	if (philo->ph_id % 2 != 0)
+	{
+		philo->left_fork = &forks_arr[(ph_index + 1) % ph_total];
+		philo->right_fork = &forks_arr[ph_index];
+	}
+	if (philo->ph_id % 2 == 0)
+	{
+		philo->left_fork = &forks_arr[ph_index];
+		philo->right_fork = &forks_arr[(ph_index + 1) % ph_total];
+	}
 }
 
 static void	philo_init(t_data *data)
