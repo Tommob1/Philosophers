446f2eeb0f1e1006ce3d2ae6f29fdf6a30edb734
Brayden Tomlinson <humortronic@gmail.com>
2024-07-18 17:27:43 +0930

    Added changes to code

--- src/data_init.c
+++ src/data_init.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:27 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/17 14:52:56 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/07/18 17:27:13 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,40 @@ static void	assign_forks(t_ph *philo, t_fork *forks_arr, int ph_index)
 
 static void	philo_init(t_data *data)
 {
+	int	i;
+	t_ph	*philo;
 
+	i = 0;
+	while (i < data->ph_total)
+	{
+		philo = data->philos_arr + i;
+		philo->ph_id = i + 1;
+		philo->max_meals = false;
+		philo->meal_count = 0;
+		handle_mutex(&philo->ph_mutex, INIT);
+		philo->data = data;
+		assign_forks(philo, data->forks_arr, i);
+		i++;
+	}
 }
 
 void	data_init(t_data *data)
 {
-	
+	int	i;
+
+	i = 0;
+	data->end_time = false;
+	data->threads_ready = false;
+	data->active_philos_count = 0;
+	data->philos_arr = ft_malloc(data->ph_total * sizeof(t_ph));
+	data->forks_arr = ft_malloc(data->ph_total * sizeof(t_fork));
+	handle_mutex(&data->write_mutex, INIT);
+	handle_mutex(&data->access_mutex, INIT);
+	while (i < data->ph_total)
+	{
+		handle_mutex(&data->forks_arr[i].fork_mutex, INIT);
+		data->forks_arr[i].fork_id = i;
+		i++;
+	}
+	philo_init(data);
 }
