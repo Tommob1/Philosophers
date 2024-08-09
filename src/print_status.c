5eab4d2afed4ed6ff108342cf3a34ae294f160ec
Brayden Tomlinson <humortronic@gmail.com>
2024-08-09 14:15:11 +0930

    Added changes to code

--- src/print_status.c
+++ src/print_status.c
@@ -6,8 +6,22 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:17 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/17 14:25:41 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/08/09 14:14:58 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
+
+void    ph_status(t_ph_status status, t_ph *philo)
+{
+    long    elapsed;
+    bool    end_time;
+
+    elapsed = gettime(MILLISECONDS) - philo->data->start_time;
+    if (get_bool(&philo->ph_mutex, &philo->max_meals))
+        return ;
+    else
+    {
+        
+    }
+}
