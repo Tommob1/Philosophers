bc7028217538519ae5d4f6629a7e2f914ca49f43
Brayden Tomlinson <humortronic@gmail.com>
2024-08-09 14:59:30 +0930

    Added changes to code

--- src/protect_vars.c
+++ src/protect_vars.c
@@ -6,8 +6,28 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:11 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/17 14:25:43 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/08/09 14:59:12 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
+
+void    set_bool(t_mtx *mutex, bool *dst, bool value)
+{
+
+}
+
+bool    get_bool(t_mtx *mutex, bool *value)
+{
+
+}
+
+void    set_long(t_mtx *mutex, long *dst, long value)
+{
+
+}
+
+long    get_long(t_mtx *mutex, long *value)
+{
+    
+}
