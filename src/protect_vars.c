e96d9ddcd4b37199e32fbfe28cb79bd409b5a09c
Brayden Tomlinson <humortronic@gmail.com>
2024-08-09 15:04:47 +0930

    Added changes to code

--- src/protect_vars.c
+++ src/protect_vars.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:11 by btomlins          #+#    #+#             */
-/*   Updated: 2024/08/09 14:59:12 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/08/09 15:04:28 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void    set_bool(t_mtx *mutex, bool *dst, bool value)
 {
-
+    handle_mutex(mutex, LOCK);
+    *dst = value;
+    handle_mutex(mutex, UNLOCK);
 }
 
 bool    get_bool(t_mtx *mutex, bool *value)
