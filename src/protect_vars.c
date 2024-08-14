c214f9900e16d31bb3b3583e511fc3ecd02e3921
Brayden Tomlinson <humortronic@gmail.com>
2024-08-14 12:46:14 +0930

    Added changes to code: '

--- src/protect_vars.c
+++ src/protect_vars.c
@@ -6,42 +6,42 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:11 by btomlins          #+#    #+#             */
-/*   Updated: 2024/08/09 15:08:47 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/08/14 12:45:49 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
 
-void    set_bool(t_mtx *mutex, bool *dst, bool value)
+void	set_bool(t_mtx *mutex, bool *dst, bool value)
 {
-    handle_mutex(mutex, LOCK);
-    *dst = value;
-    handle_mutex(mutex, UNLOCK);
+	handle_mutex(mutex, LOCK);
+	*dst = value;
+	handle_mutex(mutex, UNLOCK);
 }
 
-bool    get_bool(t_mtx *mutex, bool *value)
+bool	get_bool(t_mtx *mutex, bool *value)
 {
-    bool    retrieved;
+	bool	retrieved;
 
-    handle_mutex(mutex, LOCK);
-    retrieved = *value;
-    handle_mutex(mutex, UNLOCK);
-    return (retrieved);
+	handle_mutex(mutex, LOCK);
+	retrieved = *value;
+	handle_mutex(mutex, UNLOCK);
+	return (retrieved);
 }
 
-void    set_long(t_mtx *mutex, long *dst, long value)
+void	set_long(t_mtx *mutex, long *dst, long value)
 {
-    handle_mutex(mutex, LOCK);
-    *dst = value;
-    handle_mutex(mutex, UNLOCK);
+	handle_mutex(mutex, LOCK);
+	*dst = value;
+	handle_mutex(mutex, UNLOCK);
 }
 
-long    get_long(t_mtx *mutex, long *value)
+long	get_long(t_mtx *mutex, long *value)
 {
-    long    retrieve;
+	long	retrieve;
 
-    handle_mutex(mutex, LOCK);
-    retrieve = *value;
-    handle_mutex(mutex, UNLOCK);
-    return (retrieve);
+	handle_mutex(mutex, LOCK);
+	retrieve = *value;
+	handle_mutex(mutex, UNLOCK);
+	return (retrieve);
 }
