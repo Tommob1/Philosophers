6859ac8bf29c8f5402c097fc35416444c6b43456
Brayden Tomlinson <humortronic@gmail.com>
2024-08-14 12:32:32 +0930

    Added changes to code

--- src/monitor.c
+++ src/monitor.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 14:11:27 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/19 14:21:17 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/08/14 12:32:14 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,63 +14,66 @@
 
 void	wait_all_threads(t_data *data)
 {
-    while (!get_bool(&data->access_mutex, &data->threads_ready))
-            ;
+	while (!get_bool(&data->access_mutex, &data->threads_ready))
+		;
 }
 
-void    active_thread_counter(t_mtx *mutex, long *value)
+void	active_thread_counter(t_mtx *mutex, long *value)
 {
-    handle_mutex(mutex, LOCK);
-    (*value)++;
-    handle_mutex(mutex, UNLOCK);
+	handle_mutex(mutex, LOCK);
+	(*value)++;
+	handle_mutex(mutex, UNLOCK);
 }
 
-static bool philo_died(t_ph *philo)
+static bool	philo_died(t_ph *philo)
 {
-    long    elapsed;
-    long    dying_time;
+	long	elapsed;
+	long	dying_time;
 
-    if (get_bool(&philo->ph_mutex, &philo->max_meals))
-            return (false);
-    elapsed = gettime(MILLISECONDS) - get_long(&philo->ph_mutex, &philo->meal_time);
-    dying_time = philo->data->time_to_die / 1000;
-    if (elapsed > dying_time)
-            return (true);
-    return (false);
+	if (get_bool(&philo->ph_mutex, &philo->max_meals))
+		return (false);
+	elapsed = gettime(MILLISECONDS) - get_long(&philo->ph_mutex,
+			&philo->meal_time);
+	dying_time = philo->data->time_to_die / 1000;
+	if (elapsed > dying_time)
+		return (true);
+	return (false);
 }
 
-static bool all_philos_active(t_mtx *mutex, long *threads, long ph_total)
+static bool	all_philos_active(t_mtx *mutex, long *threads, long ph_total)
 {
-    bool    value;
+	bool	value;
 
-    value = false;
-    handle_mutex(mutex, LOCK);
-    if (*threads == ph_total)
-            value = true;
-    handle_mutex(mutex, UNLOCK);
-    return (value);
+	value = false;
+	handle_mutex(mutex, LOCK);
+	if (*threads == ph_total)
+		value = true;
+	handle_mutex(mutex, UNLOCK);
+	return (value);
 }
 
-void    *death_affirm(void *ph_data)
+void	*death_affirm(void *ph_data)
 {
-    int     i;
-    t_data  *data;
+	int		i;
+	t_data	*data;
 
-    data = (t_data *)ph_data;
-    while (!all_philos_active(&data->access_mutex, &data->active_philos_count, data->ph_total))
-            ;
-    while (!get_bool(&data->access_mutex, &data->end_time))
-    {
-        i = 0;
-        while (i < data->ph_total && !get_bool(&data->access_mutex, &data->end_time))
-        {
-            if (philo_died(data->philos_arr + i))
-            {
-                set_bool(&data->access_mutex, &data->end_time, true);
-                ph_status(DIED, data->philos_arr + i);
-            }
-            i++;
-        }
-    }
-    return (NULL);
+	data = (t_data *)ph_data;
+	while (!all_philos_active(&data->access_mutex, &data->active_philos_count,
+			data->ph_total))
+		;
+	while (!get_bool(&data->access_mutex, &data->end_time))
+	{
+		i = 0;
+		while (i < data->ph_total && !get_bool(&data->access_mutex,
+				&data->end_time))
+		{
+			if (philo_died(data->philos_arr + i))
+			{
+				set_bool(&data->access_mutex, &data->end_time, true);
+				ph_status(DIED, data->philos_arr + i);
+			}
+			i++;
+		}
+	}
+	return (NULL);
 }
