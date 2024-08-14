13d47c6bab657a334ee0cb9b395b994dc7480ae4
Brayden Tomlinson <humortronic@gmail.com>
2024-08-14 16:05:06 +0930

    added changes to code

--- src/sim_start.c
+++ src/sim_start.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 14:10:20 by btomlins          #+#    #+#             */
-/*   Updated: 2024/08/14 12:26:59 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/08/14 16:04:51 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 
 static void thinking(t_ph *philo, bool pre_simulation)
 {
+	long	eating_time;
+	long	sleeping_time;
+	long	thinking_time;
 
+	if (!pre_simulation)
+		ph_status(THINKING, philo);
+	if (philo->data->ph_total % 2 == 0)
+		return ;
+	eating_time = philo->data->time_to_eat;
+	sleeping_time = philo->data->time_to_sleep;
+	thinking_time = (eating_time * 2) - sleeping_time;
+	if (thinking_time < 0)
+		thinking_time = 0;
+	ft_usleep(thinking_time * 0.3, philo->data);
 }
 
 static void synchronize_dining(t_ph *philo)
