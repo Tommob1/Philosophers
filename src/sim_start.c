e2dc931757b192c100dc3ee374183c9ee9006530
Brayden Tomlinson <humortronic@gmail.com>
2024-08-14 12:27:14 +0930

    Added changes to code

--- src/sim_start.c
+++ src/sim_start.c
@@ -6,8 +6,33 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 14:10:20 by btomlins          #+#    #+#             */
-/*   Updated: 2024/07/17 14:25:47 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/08/14 12:26:59 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
+
+static void thinking(t_ph *philo, bool pre_simulation)
+{
+
+}
+
+static void synchronize_dining(t_ph *philo)
+{
+
+}
+
+static void eating(t_ph *philo)
+{
+
+}
+
+static void *dining_philos(void *ph_data)
+{
+
+}
+
+void    sim_start(t_data *data)
+{
+
+}
