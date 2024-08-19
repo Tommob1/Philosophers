5bce6e2c954b5042f60befe77e0656b4e6ce75bd
Brayden Tomlinson <humortronic@gmail.com>
2024-08-19 16:49:41 +0930

    Added changes to code

--- src/print_status.c
+++ src/print_status.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:45:17 by btomlins          #+#    #+#             */
-/*   Updated: 2024/08/14 13:56:49 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/08/19 16:49:20 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,16 @@ void	ph_status(t_ph_status status, t_ph *philo)
 		end_time = get_bool(&philo->data->access_mutex, &philo->data->end_time);
 		if ((status == TAKES_LEFTFORK || status == TAKES_RIGHTFORK)
 			&& !end_time)
-			printf(WHITE"%-61d"RESET" %d has taken a fork\n", elapsed,
+			printf(WHITE"%-6ld"RESET" %d has taken a fork\n", elapsed,
 				philo->ph_id);
 		else if (status == EATING && !end_time)
-			printf(WHITE"%-61d"GREEN" %d is eating\n", elapsed, philo->ph_id);
+			printf(WHITE"%-6ld"GREEN" %d is eating\n"RESET, elapsed, philo->ph_id);
 		else if (status == SLEEPING && !end_time)
-			printf(WHITE"%-61d"RESET" %d is sleeping\n", elapsed, philo->ph_id);
+			printf(WHITE"%-6ld"RESET" %d is sleeping\n", elapsed, philo->ph_id);
 		else if (status == THINKING && !end_time)
-			printf(WHITE"%-61d"RESET" %d is thinking\n", elapsed, philo->ph_id);
+			printf(WHITE"%-6ld"RESET" %d is thinking\n", elapsed, philo->ph_id);
 		else if (status == DIED)
-			printf(RED"%-61d %d died\n"RESET, elapsed, philo->ph_id);
+			printf(RED"%-6ld %d died\n"RESET, elapsed, philo->ph_id);
 	}
 	handle_mutex(&philo->data->write_mutex, UNLOCK);
 }
